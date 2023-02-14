68345037da584dfaec27636c5ac39c84c28142ec
emersonv25 <emersondejesussantos@hotmail.com>
2023-02-13 21:30:22 -0300

    Exibir mensagem de video indisponivel

--- src/components/cards/CardWatch.tsx
+++ src/components/cards/CardWatch.tsx
@@ -9,34 +9,49 @@ type Props = {
 export default function CardWatch({ episode }: Props) {
     const [videoUrl, setVideoUrl] = useState('')
 
+    const isVideo = () => {
+        if (typeof episode.episodeVideos === "undefined" || episode.episodeVideos.length == 0) {
+            return false
+        } else { return true }
+    }
+
     useEffect(() => {
-        if(typeof episode.episodeVideos !== 'undefined')
-        {
+        if (typeof episode.episodeVideos !== 'undefined' && episode.episodeVideos.length > 0) {
             setVideoUrl(episode.episodeVideos[0].videoUrl)
         }
     }, [episode])
+
+
     return (
         <>
             <Card sx={{ display: 'flex', background: 'transparent', justifyContent: 'start' }} elevation={0}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', width: 1.0 }}>
-                    <Box sx={{ display: 'flex', height: { xs: 280, sm: 500, md: 600, lg: 700 } }}>
-                        <iframe
-                            width='100%'
-                            height='100%'
-                            frameBorder={0}
-                            scrolling='no'
-                            allow='fullscreen'
-                            src={videoUrl} />
-                    </Box>
+                    {
+                        isVideo() && <Box sx={{ display: 'flex', height: { xs: 280, sm: 500, md: 600, lg: 700 } }}>
+                            <iframe
+                                width='100%'
+                                height='100%'
+                                frameBorder={0}
+                                scrolling='no'
+                                allow='fullscreen'
+                                src={videoUrl} />
+                        </Box>
+                    }
+                    {
+                        !isVideo() && <Box sx={{ textAlign: 'center', background: 'black' }}><h2>Vídeo indisponível</h2></Box> 
+                    }
 
-                    <Box sx={{ textAlign: 'center', background: 'black' }}>
-                        <ButtonGroup variant='text'>
-                            {episode.episodeVideos&& episode.episodeVideos.map((episodeVideo, index) => <Button sx={{margin: 1}} variant="contained" size="small" color='primary' onClick={() => setVideoUrl(episodeVideo.videoUrl)} key={index}> {episodeVideo.optionName ? episodeVideo.optionName : 'Player ' + index+1}</Button>)}
-                            <IconButton sx={{ color: 'white' }} title="Episódio Anterior" component={Link} href={`/watch/${episode.previousEpisodeKey}`} disabled={episode.previousEpisodeKey == null}><Icon fontSize="large">skip_previous</Icon></IconButton>
-                            <IconButton sx={{ color: 'white' }} title='Todos os Episódios' component={Link} href={`/serie/${episode.serieKey}`}><Icon fontSize="large">list</Icon></IconButton>
-                            <IconButton sx={{ color: 'white' }} title='Próximo Episódio' component={Link} href={`/watch/${episode.nextEpisodeKey}`} disabled={episode.nextEpisodeKey == null}><Icon fontSize="large">skip_next</Icon></IconButton>
-                        </ButtonGroup>
-                    </Box>
+                    {
+                        <Box sx={{ textAlign: 'center', background: 'black' }}>
+                            <ButtonGroup variant='text'>
+                                {episode.episodeVideos && episode.episodeVideos.map((episodeVideo, index) => <Button sx={{ margin: 1 }} variant="contained" size="small" color='primary' onClick={() => setVideoUrl(episodeVideo.videoUrl)} key={index}> {episodeVideo.optionName ? episodeVideo.optionName : 'Player ' + index + 1}</Button>)}
+                                <IconButton sx={{ color: 'white' }} title="Episódio Anterior" component={Link} href={`/watch/${episode.previousEpisodeKey}`} disabled={episode.previousEpisodeKey == null}><Icon fontSize="large">skip_previous</Icon></IconButton>
+                                <IconButton sx={{ color: 'white' }} title='Todos os Episódios' component={Link} href={`/serie/${episode.serieKey}`}><Icon fontSize="large">list</Icon></IconButton>
+                                <IconButton sx={{ color: 'white' }} title='Próximo Episódio' component={Link} href={`/watch/${episode.nextEpisodeKey}`} disabled={episode.nextEpisodeKey == null}><Icon fontSize="large">skip_next</Icon></IconButton>
+                            </ButtonGroup>
+                        </Box>
+                    }
+
                     <Box>
                         <CardContent>
                             <Typography sx={{ textAlign: 'start' }} component="div" variant="h5">
