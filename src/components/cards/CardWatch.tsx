1d2ef5a5a9f12dd56d5044ab66b55d3cb51349f5
emersonv25 <emersondejesussantos@hotmail.com>
2023-03-05 22:23:14 -0300

    added tag video

--- src/components/cards/CardWatch.tsx
+++ src/components/cards/CardWatch.tsx
@@ -8,6 +8,7 @@ type Props = {
 }
 export default function CardWatch({ episode }: Props) {
     const [videoUrl, setVideoUrl] = useState('')
+    const [isIframe, setIsIframe] = useState(true)
 
     const isVideo = () => {
         if (typeof episode.episodeVideos === "undefined" || episode.episodeVideos.length == 0) {
@@ -18,6 +19,7 @@ export default function CardWatch({ episode }: Props) {
     useEffect(() => {
         if (typeof episode.episodeVideos !== 'undefined' && episode.episodeVideos.length > 0) {
             setVideoUrl(episode.episodeVideos[0].videoUrl)
+            setIsIframe(episode.episodeVideos[0].isIframe)
         }
     }, [episode])
 
@@ -27,24 +29,37 @@ export default function CardWatch({ episode }: Props) {
             <Card sx={{ display: 'flex', background: 'transparent', justifyContent: 'start' }} elevation={0}>
                 <Box sx={{ display: 'flex', flexDirection: 'column', width: 1.0 }}>
                     {
-                        isVideo() && <Box sx={{ display: 'flex', height: { xs: 280, sm: 500, md: 600, lg: 700 } }}>
-                            <iframe
-                                width='100%'
-                                height='100%'
-                                frameBorder={0}
-                                scrolling='no'
-                                allow='fullscreen'
-                                src={videoUrl} />
+                        isVideo() &&
+
+                        <Box sx={{ display: 'flex', height: { xs: 280, sm: 500, md: 600, lg: 700 } }}>
+                            {
+                                isIframe ?
+                                    <iframe
+                                        width='100%'
+                                        height='100%'
+                                        frameBorder={0}
+                                        scrolling='no'
+                                        allow='fullscreen'
+                                        src={videoUrl} />
+                                    :
+                                    <video
+                                        controls
+                                        src={videoUrl}
+                                        width='100%'
+                                        height='100%'
+                                    ></video>
+                            }
+
                         </Box>
                     }
                     {
-                        !isVideo() && <Box sx={{ textAlign: 'center', background: 'black' }}><h2>Vídeo indisponível</h2></Box> 
+                        !isVideo() && <Box sx={{ textAlign: 'center', background: 'black' }}><h2>Vídeo indisponível</h2></Box>
                     }
 
                     {
                         <Box sx={{ textAlign: 'center', background: 'black' }}>
                             <ButtonGroup variant='text'>
-                                {episode.episodeVideos && episode.episodeVideos.map((episodeVideo, index) => <Button sx={{ margin: 1 }} variant="contained" size="small" color='primary' onClick={() => setVideoUrl(episodeVideo.videoUrl)} key={index}> {episodeVideo.optionName ? episodeVideo.optionName : 'Player ' + index + 1}</Button>)}
+                                {episode.episodeVideos && episode.episodeVideos.map((episodeVideo, index) => <Button sx={{ margin: 1 }} variant="contained" size="small" color='primary' onClick={() => { setVideoUrl(episodeVideo.videoUrl); setIsIframe(episodeVideo.isIframe) }} key={index}> {episodeVideo.optionName ? episodeVideo.optionName : 'Player ' + index + 1}</Button>)}
                                 <IconButton sx={{ color: 'white' }} title="Episódio Anterior" component={Link} href={`/watch/${episode.previousEpisodeKey}`} disabled={episode.previousEpisodeKey == null}><Icon fontSize="large">skip_previous</Icon></IconButton>
                                 <IconButton sx={{ color: 'white' }} title='Todos os Episódios' component={Link} href={`/serie/${episode.serieKey}`}><Icon fontSize="large">list</Icon></IconButton>
                                 <IconButton sx={{ color: 'white' }} title='Próximo Episódio' component={Link} href={`/watch/${episode.nextEpisodeKey}`} disabled={episode.nextEpisodeKey == null}><Icon fontSize="large">skip_next</Icon></IconButton>
